f044030271371ad35db87ae5afcb3d7c180644a3
David Bayendor <dbayendor@gmail.com>
2014-10-06 20:52:38 -0600

    Add item class & test

---
+++ test/item_test.rb
@@ -0,0 +1,27 @@
+require_relative 'test_helper'
+
+require_relative '../lib/item'
+
+class ItemTest < MiniTest::Test
+  def test_item_attributes
+    data = {
+      id: '1',
+      name: 'Item Qui Esse',
+      description: 'Nihil autem sit odio inventore deleniti.',
+      unit_price: '75107',
+      merchant_id: '1',
+      created_at: '2012-03-27 14:53:59 UTC',
+      updated_at: '2012-03-27 14:53:59 UTC'
+    }
+
+    item = Item.new(data)
+
+    assert_equal '1', item.id
+    assert_equal 'Item Qui Esse', item.name
+    assert_equal 'Nihil autem sit odio inventore deleniti.', item.description
+    assert_equal '75107', item.unit_price
+    assert_equal '1', item.merchant_id
+    assert_equal '2012-03-27 14:53:59 UTC', item.created_at
+    assert_equal '2012-03-27 14:53:59 UTC', item.updated_at
+  end
+end
