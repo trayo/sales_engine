6c6d7d77efa1cdbc7414693f7e9a09a93f274cea
David Bayendor <dbayendor@gmail.com>
2014-10-07 17:14:13 -0600

    Initial tests & file build

---
+++ test/merchant_repository_test.rb
@@ -0,0 +1,88 @@
+require_relative 'test_helper'
+
+require_relative '../lib/merchant_repository'
+
+class MerchantRepositoryTest < Minitest::Test
+
+  def setup
+    merchants = [
+      {
+      id: '1',
+      first_name: 'Joey',
+      last_name: 'Ondricka',
+      created_at: '2012-03-27 14:54:09 UTC',
+      updated_at: '2012-03-27 14:54:09 UTC'
+      },
+      {
+      id: '2',
+      first_name: 'Cecelia',
+      last_name: 'Osinski',
+      created_at: '2012-03-27 14:54:10 UTC',
+      updated_at: '2012-03-27 14:54:10 UTC'
+      },
+      {
+      id: '3',
+      first_name: 'Mariah',
+      last_name: 'Toy',
+      created_at: '2012-03-27 14:54:10 UTC',
+      updated_at: '2012-03-27 14:54:10 UTC'
+      },
+      {
+      id: '4',
+      first_name: 'Mariah',
+      last_name: 'Osinski',
+      created_at: '2012-03-27 14:54:10 UTC',
+      updated_at: '2012-03-27 14:54:10 UTC'
+      },
+      {
+      id: '5',
+      first_name: 'Cecelia',
+      last_name: 'Toy',
+      created_at: '2012-03-27 14:54:10 UTC',
+      updated_at: '2012-03-27 14:54:10 UTC'
+      }
+    ].map {|row| Merchant.new(row)}
+
+    @merchant_repo = MerchantRepository.new(merchants)
+  end
+
+  def test_find_all
+    skip
+    assert_equal 5, @merchant_repo.all.size
+  end
+
+  def test_find_by_id
+    skip
+    merchant = @merchant_repo.find_by_id(2)
+    assert_equal 2, merchant.id
+  end
+
+  def test_find_by_first_name
+    skip
+    merchant = @merchant_repo.find_by_first_name('Mariah')
+    assert_equal 'Mariah', merchant.first_name
+  end
+
+  def test_find_by_last_name
+    skip
+    merchant = @merchant_repo.find_by_last_name('Toy')
+    assert_equal 'Toy', merchant.last_name
+  end
+
+  def test_find_all_by_first_name
+    skip
+    assert_equal 2, @merchant_repo.find_all_by_first_name('Mariah').size
+  end
+
+  def test_find_all_by_last_name
+    skip
+    assert_equal 2, @merchant_repo.find_all_by_last_name('Toy').size
+  end
+
+  def test_find_random
+    skip
+    assert_class = Merchant, @merchant_repo.random
+  end
+end
+
+#If there is no match, it returns an empty Array.
