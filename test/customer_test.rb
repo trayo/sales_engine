fb0683284604f6e12a3fb04f1fac3e361217e7a1
Travis Yoder <trayoda@gmail.com>
2014-10-07 21:26:18 -0600

    Changed customer to introduce some basic relationship logic.

--- test/customer_test.rb
+++ test/customer_test.rb
@@ -1,9 +1,10 @@
 require_relative 'test_helper'
 
 require_relative '../lib/customer'
+require_relative '../lib/invoice_repository'
 
 class CustomerTest < MiniTest::Test
-  def test_customer_attributes
+  def setup
     data = {
       id: '1',
       first_name: 'Joey',
@@ -12,12 +13,33 @@ class CustomerTest < MiniTest::Test
       updated_at: '2012-03-27 14:54:09 UTC'
     }
 
-    customer = Customer.new(data)
+    @customer = Customer.new(data)
+  end
 
-    assert_equal 1, customer.id
-    assert_equal 'Joey', customer.first_name
-    assert_equal 'Ondricka', customer.last_name
-    assert_equal '2012-03-27 14:54:09 UTC', customer.created_at
-    assert_equal '2012-03-27 14:54:09 UTC', customer.updated_at
+  def test_customer_attributes
+    assert_equal 1, @customer.id
+    assert_equal 'Joey', @customer.first_name
+    assert_equal 'Ondricka', @customer.last_name
+    assert_equal '2012-03-27 14:54:09 UTC', @customer.created_at
+    assert_equal '2012-03-27 14:54:09 UTC', @customer.updated_at
+  end
+
+  def test_invoices
+    invoice_data = [
+      {
+      id: '1',
+      customer_id: '1',
+      merchant_id: '26',
+      status: 'shipped',
+      created_at: '2012-03-27 14:54:09 UTC',
+      updated_at: '2012-03-27 14:54:09 UTC'
+      }
+    ].map {|row| Invoice.new(row)}
+    invoice_repository = InvoiceRepository.new(invoice_data)
+    @customer.set_invoice_repository(invoice_repository)
+
+    only_invoice = @customer.invoices.first
+    assert_equal 1, only_invoice.id
+    assert_equal 26, only_invoice.merchant_id
   end
 end
