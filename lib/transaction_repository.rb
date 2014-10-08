23ac7d9cf60dc1ba89affe349b68f9c8f25a05e7
David Bayendor <dbayendor@gmail.com>
2014-10-08 14:41:47 -0600

    Add transaction repository

---
+++ lib/transaction_repository.rb
@@ -0,0 +1,57 @@
+require_relative 'transaction'
+
+class TransactionRepository
+
+  attr_reader :transactions
+
+  def initialize(transactions = [])
+    @transactions = transactions
+  end
+
+  def all
+    transactions
+  end
+
+  def random
+    transactions.sample
+  end
+
+  def find_by_id(id)
+    find_by_attribute(:id, id)
+  end
+
+  def find_by_invoice_id(invoice_id)
+    find_by_attribute(:invoice_id, invoice_id)
+  end
+
+  def find_by_credit_card_number(credit_card_number)
+    find_by_attribute(:credit_card_number, credit_card_number)
+  end
+
+  def find_by_credit_card_expiration_date(credit_card_expiration_date)
+    find_by_attribute(:credit_card_expiration_date, credit_card_expiration_date)
+  end
+
+  def find_all_by_invoice_id(invoice_id)
+    find_all_by_attribute(:invoice_id, invoice_id)
+  end
+
+  def find_all_by_credit_card_number(credit_card_number)
+    find_all_by_attribute(:credit_card_number, credit_card_number)
+  end
+
+  def find_all_by_credit_card_expiration_date(credit_card_expiration_date)
+    find_all_by_attribute(:credit_card_expiration_date, credit_card_expiration_date)
+  end
+
+  private
+
+  def find_by_attribute(attribute, value)
+    transactions.find {|transaction| transaction.public_send(attribute) == value}
+  end
+
+  def find_all_by_attribute(attribute, value)
+    transactions.find_all {|transaction| transaction.public_send(attribute) == value}
+  end
+
+end
---
+++ test/transaction_repository_test.rb
@@ -0,0 +1,102 @@
+require_relative 'test_helper'
+
+require_relative '../lib/transaction_repository'
+
+class TransactionRepositoryTest < Minitest::Test
+
+  def setup
+    transactions = [
+      {
+        id: '1',
+        invoice_id: '1',
+        credit_card_number: '4844518708741275',
+        credit_card_expiration_date: '',
+        result: 'success',
+        created_at: '2012-03-27 14:54:09 UTC',
+        updated_at: '2012-03-27 14:54:09 UTC'
+      },
+      {
+        id: '2',
+        invoice_id: '2',
+        credit_card_number: '4580251236515201',
+        credit_card_expiration_date: '',
+        result: 'success',
+        created_at: '2012-03-27 14:54:09 UTC',
+        updated_at: '2012-03-27 14:54:09 UTC'
+      },
+      {
+        id: '3',
+        invoice_id: '3',
+        credit_card_number: '4354495077693036',
+        credit_card_expiration_date: '',
+        result: 'success',
+        created_at: '2012-03-27 14:54:09 UTC',
+        updated_at: '2012-03-27 14:54:09 UTC'
+      },
+      {
+        id: '4',
+        invoice_id: '3',
+        credit_card_number: '4654405418249632',
+        credit_card_expiration_date: '2012-03-27',
+        result: 'failed',
+        created_at: '2012-03-27 14:54:09 UTC',
+        updated_at: '2012-03-27 14:54:09 UTC'
+      },
+      {
+        id: '5',
+        invoice_id: '4',
+        credit_card_number: '4654405418249632',
+        credit_card_expiration_date: '',
+        result: 'failed',
+        created_at: '2012-03-27 14:54:09 UTC',
+        updated_at: '2012-03-27 14:54:09 UTC'
+    }
+    ].map {|row| Transaction.new(row)}
+
+    @transaction_repo = TransactionRepository.new(transactions)
+  end
+
+  def test_all
+    assert_equal 5, @transaction_repo.all.size
+  end
+
+  def test_random
+    assert_class = Transaction, @transaction_repo.random
+  end
+
+  def test_find_by_id
+    transaction = @transaction_repo.find_by_id('4')
+    assert_equal '4', transaction.id
+  end
+
+  def test_find_by_invoice_id
+    transaction = @transaction_repo.find_by_invoice_id('2')
+    assert_equal '2', transaction.invoice_id
+  end
+
+  def test_find_by_credit_card_number
+    transaction = @transaction_repo.find_by_credit_card_number('4580251236515201')
+    assert_equal '4580251236515201', transaction.credit_card_number
+  end
+
+  def test_find_by_credit_card_expiration_date
+    transaction = @transaction_repo.find_by_credit_card_expiration_date('2012-03-27')
+    assert_equal '2012-03-27', transaction.credit_card_expiration_date
+  end
+
+  def test_find_all_by_credit_card_number
+    transactions = @transaction_repo.find_all_by_credit_card_number('4654405418249632')
+    assert_equal 2, transactions.size
+  end
+
+  def test_find_all_by_credit_card_expiration_date
+    transactions = @transaction_repo.find_all_by_credit_card_expiration_date('')
+    assert_equal 4, transactions.size
+  end
+
+  def test_find_all_by_invoice_id_returns_empty_array
+    empty_transactions = @transaction_repo.find_all_by_invoice_id("-2")
+    assert_equal [], empty_transactions
+  end
+
+end
