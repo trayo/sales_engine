a8ba59f37b5be48a719c3a47b4c0ef27ddc4a0df
Travis Yoder <trayoda@gmail.com>
2014-10-08 17:40:24 -0600

    added some missing tests and methods for extracting data. moved methods to match style of other tests and classes

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -12,6 +12,10 @@ class ItemRepository
     items
   end
 
+  def random
+    items.sample
+  end
+
   def find_by_id(id)
     find_by_attribute(:id, id)
   end
@@ -20,28 +24,32 @@ class ItemRepository
     find_by_attribute(:name, name)
   end
 
-  def find_by_merchant_id(merchant_id)
-    find_by_attribute(:merchant_id, merchant_id)
+  def find_all_by_name(name)
+    find_all_by_attribute(:name, name)
   end
 
   def find_by_description(description)
     find_by_attribute(:description, description)
   end
 
-  def find_by_unit_price(unit_price)
-    find_by_attribute(:unit_price, unit_price)
-  end
-
-  def find_all_by_merchant_id(merchant_id)
-    find_all_by_attribute(:merchant_id, merchant_id)
-  end
-
   def find_all_by_description(description)
     find_all_by_attribute(:description, description)
   end
 
-  def random
-    items.sample
+  def find_by_unit_price(unit_price)
+    find_by_attribute(:unit_price, unit_price)
+  end
+
+  def find_all_by_unit_price(unit_price)
+    find_all_by_attribute(:unit_price, unit_price)
+  end
+
+  def find_by_merchant_id(merchant_id)
+    find_by_attribute(:merchant_id, merchant_id)
+  end
+
+  def find_all_by_merchant_id(merchant_id)
+    find_all_by_attribute(:merchant_id, merchant_id)
   end
 
   private
