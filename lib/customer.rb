f9ec7413b4f4b18d6ac51a66488f232b85c43e62
Travis Yoder <trayoda@gmail.com>
2014-10-09 11:39:12 -0600

    added downcase to first name and last name

--- lib/customer.rb
+++ lib/customer.rb
@@ -6,9 +6,9 @@ class Customer
               :updated_at
 
   def initialize(data)
-    @id = data[:id].to_i
-    @first_name = data[:first_name]
-    @last_name = data[:last_name]
+    @id         = data[:id].to_i
+    @first_name = data[:first_name].downcase
+    @last_name  = data[:last_name].downcase
     @created_at = data[:created_at]
     @updated_at = data[:updated_at]
   end
--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -12,27 +12,27 @@ class CustomerRepository
     customers
   end
 
+  def random
+    customers.sample
+  end
+
   def find_by_id(id)
     customers.find {|customer| customer.id == id}
   end
 
   def find_by_first_name(first_name)
-    customers.find {|customer| customer.first_name == first_name}
-  end
-
-  def find_by_last_name(last_name)
-    customers.find {|customer| customer.last_name == last_name}
+    customers.find {|customer| customer.first_name == first_name.downcase}
   end
 
   def find_all_by_first_name(first_name)
-    customers.find_all {|customer| customer.first_name == first_name}
+    customers.find_all {|customer| customer.first_name == first_name.downcase}
+  end
+
+  def find_by_last_name(last_name)
+    customers.find {|customer| customer.last_name == last_name.downcase}
   end
 
   def find_all_by_last_name(last_name)
-    customers.find_all {|customer| customer.last_name == last_name}
-  end
-
-  def random
-    customers.sample
+    customers.find_all {|customer| customer.last_name == last_name.downcase}
   end
 end
