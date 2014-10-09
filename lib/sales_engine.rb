ffcef6c51d731500a2410869392ce3aef8852022
Travis Yoder <trayoda@gmail.com>
2014-10-09 09:32:47 -0600

    setup basic sales_engine files

---
+++ lib/sales_engine.rb
@@ -0,0 +1,20 @@
+require 'csv'
+require_relative 'file_loader.rb'
+require_relative 'merchant'
+require_relative 'merchant_repository'
+require_relative 'customer.rb'
+require_relative 'customer_repository.rb'
+require_relative 'invoice.rb'
+require_relative 'invoice_repository.rb'
+require_relative 'invoice_item.rb'
+require_relative 'invoice_item_repository.rb'
+require_relative 'item.rb'
+require_relative 'item_repository.rb'
+require_relative 'transaction.rb'
+require_relative 'transaction_repository.rb'
+
+class SalesEngine
+
+  
+
+end
---
+++ test/sales_engine_test.rb
@@ -0,0 +1,16 @@
+require_relative 'test_helper'
+require_relative '../lib/sales_engine'
+
+class SalesEngineTest < Minitest::Test
+
+  def test_it_exists
+    assert SalesEngine
+  end
+
+  def setup
+
+  end
+
+  
+
+end
