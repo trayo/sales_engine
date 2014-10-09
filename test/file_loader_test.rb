616585e5338979738e962b3bf8a7054c7f2378e9
Travis Yoder <trayoda@gmail.com>
2014-10-09 11:23:18 -0600

    Updated file loader to not pad the data/ in front of file names

--- test/file_loader_test.rb
+++ test/file_loader_test.rb
@@ -2,10 +2,8 @@ require_relative 'test_helper'
 require_relative '../lib/file_loader'
 
 class FileLoaderTest < Minitest::Test
-
   def test_loads_a_file
-    file = FileLoader.load_file("customers.csv")
+    file = FileLoader.load_file("data/customers.csv")
     assert_equal "data/customers.csv", file.path
   end
-
 end
