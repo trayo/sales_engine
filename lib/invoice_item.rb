ce9efad674f7388580379464475cd932ac92a6fa
Travis Yoder <trayoda@gmail.com>
2014-10-09 13:09:25 -0600

    changed some data to integers for the spec handler

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -9,8 +9,8 @@ class InvoiceItem
 
   def initialize(data)
     @id         = data[:id].to_i
-    @item_id    = data[:item_id]
-    @invoice_id = data[:invoice_id]
+    @item_id    = data[:item_id].to_i
+    @invoice_id = data[:invoice_id].to_i
     @quantity   = data[:quantity].to_i
     @unit_price = data[:unit_price]
     @created_at = data[:created_at]
--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -20,8 +20,8 @@ class InvoiceItemTest < MiniTest::Test
 
   def test_invoice_item_attributes
     assert_equal 1, @invoice_item.id
-    assert_equal '1', @invoice_item.item_id
-    assert_equal '539', @invoice_item.invoice_id
+    assert_equal 1, @invoice_item.item_id
+    assert_equal 539, @invoice_item.invoice_id
     assert_equal 1, @invoice_item.quantity
     assert_equal '13635', @invoice_item.unit_price
     assert_equal '2012-03-27 14:54:09 UTC', @invoice_item.created_at
