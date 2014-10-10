f311ae8c47f4d99185e7936a34cc7053bc985d33
Travis Yoder <trayoda@gmail.com>
2014-10-10 08:13:01 -0600

    Made invoice accept a repository
    Made invoice_repository load a file and accept an engine
    
    Wrote tests to test the load file method of invoice_repository

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -6,12 +6,13 @@ class Invoice
               :created_at,
               :updated_at
 
-  def initialize(data)
+  def initialize(data, repository)
     @id          = data[:id].to_i
     @customer_id = data[:customer_id].to_i
     @merchant_id = data[:merchant_id].to_i
     @status      = data[:status]
     @created_at  = data[:created_at]
     @updated_at  = data[:updated_at]
+    @repository  = repository
   end
 end
--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -1,11 +1,13 @@
 require_relative 'invoice'
+require_relative 'file_loader'
 
 class InvoiceRepository
 
-  attr_reader :invoices
+  attr_reader :invoices, :engine
 
-  def initialize(invoices = [])
-    @invoices = invoices
+  def initialize(engine, invoices = "")
+    @engine = engine
+    invoices.class == Array ? @invoices = invoices : @invoices = load_file(invoices)
   end
 
   def all
@@ -17,34 +19,39 @@ class InvoiceRepository
   end
 
   def find_by_id(id)
-    invoices.find {|item| item.id == id}
+    invoices.find {|invoice| invoice.id == id}
   end
 
   def find_by_customer_id(customer_id)
-    invoices.find {|item| item.customer_id == customer_id}
+    invoices.find {|invoice| invoice.customer_id == customer_id}
   end
 
   def find_all_by_customer_id(customer_id)
-    invoices.find_all {|item| item.customer_id == customer_id}
+    invoices.find_all {|invoice| invoice.customer_id == customer_id}
   end
 
   def find_by_merchant_id(merchant_id)
-    invoices.find {|item| item.merchant_id == merchant_id}
+    invoices.find {|invoice| invoice.merchant_id == merchant_id}
   end
 
   def find_all_by_merchant_id(merchant_id)
-    invoices.find_all {|item| item.merchant_id == merchant_id}
+    invoices.find_all {|invoice| invoice.merchant_id == merchant_id}
   end
 
   def find_by_status(status)
-    invoices.find {|item| item.status == status}
+    invoices.find {|invoice| invoice.status == status}
   end
 
   def find_all_by_status(status)
-    invoices.find_all {|item| item.status == status}
+    invoices.find_all {|invoice| invoice.status == status}
   end
 
   def inspect
     "#<#{self.class} #{invoices.size} rows>"
+  end
+
+  def load_file(filepath)
+    contents = FileLoader.load_file(filepath)
+    contents.map {|row| Invoice.new(row, self)}
   end
 end
