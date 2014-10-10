62bf27270154c8ae5b1470fe446654e5ab519c98
David Bayendor <dbayendor@gmail.com>
2014-10-09 21:53:42 -0600

    Add new file load, initialize

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -1,12 +1,18 @@
 require_relative 'invoice_item'
+require_relative 'file_loader'
 
 class InvoiceItemRepository
 
   attr_reader :invoice_items
 
-  def initialize(invoice_items = [])
-    @invoice_items = invoice_items
+  def initialize(engine, path)
+    @engine        = engine
+    @invoice_items = load(path + "/invoice_items.csv")
   end
+
+  # def initialize(invoice_items = [])
+  #   @invoice_items = invoice_items
+  # end
 
   def all
     invoice_items
@@ -50,6 +56,11 @@ class InvoiceItemRepository
 
   def find_all_by_unit_price(unit_price)
     invoice_items.find_all {|invoice_item| invoice_item.unit_price == unit_price}
+  end
+
+  def load(filepath)
+    contents = FileLoader.load_file(filepath)
+    contents.map { |row| InvoiceItem.new(row) }
   end
 
   private
