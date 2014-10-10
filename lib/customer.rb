c032a785dd69ef91dff4c26cdff71b38c64d64c5
David Bayendor <dbayendor@gmail.com>
2014-10-09 20:42:08 -0600

    Customer data no longer down cased
    * customer_test also passing
    
    * customer_repository tests failing
    
    * passes in spec_harness

--- lib/customer.rb
+++ lib/customer.rb
@@ -7,8 +7,8 @@ class Customer
 
   def initialize(data)
     @id         = data[:id].to_i
-    @first_name = data[:first_name].downcase
-    @last_name  = data[:last_name].downcase
+    @first_name = data[:first_name]
+    @last_name  = data[:last_name]
     @created_at = data[:created_at]
     @updated_at = data[:updated_at]
   end
--- test/customer_test.rb
+++ test/customer_test.rb
@@ -18,8 +18,8 @@ class CustomerTest < MiniTest::Test
 
   def test_customer_attributes
     assert_equal 1, @customer.id
-    assert_equal 'Joey'.downcase, @customer.first_name
-    assert_equal 'Ondricka'.downcase, @customer.last_name
+    assert_equal 'Joey', @customer.first_name
+    assert_equal 'Ondricka', @customer.last_name
     assert_equal '2012-03-27 14:54:09 UTC', @customer.created_at
     assert_equal '2012-03-27 14:54:09 UTC', @customer.updated_at
   end
