c5276e6f60e9af468bf7bbbf7ab74e0c2683ca98
David Bayendor <dbayendor@gmail.com>
2014-10-09 21:11:29 -0600

    Updated to use regular find methods
    * Added new initialize for spec_harness
    
    * Passes regular tests

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -1,8 +1,14 @@
 require_relative 'transaction'
+require_relative 'file_loader'
 
 class TransactionRepository
 
   attr_reader :transactions
+
+  # def initialize(engine, path)
+  #   @engine       = engine
+  #   @transactions = load(path + "/transactions.csv")
+  # end
 
   def initialize(transactions = [])
     @transactions = transactions
@@ -17,50 +23,42 @@ class TransactionRepository
   end
 
   def find_by_id(id)
-    find_by_attribute(:id, id)
+    transactions.find {|transaction| transaction.id == id}
   end
 
   def find_by_invoice_id(invoice_id)
-    find_by_attribute(:invoice_id, invoice_id)
+    transactions.find {|transaction| transaction.invoice_id == invoice_id}
   end
 
   def find_all_by_invoice_id(invoice_id)
-    find_all_by_attribute(:invoice_id, invoice_id)
+    transactions.find_all {|transaction| transaction.invoice_id == invoice_id}
   end
 
   def find_by_credit_card_number(credit_card_number)
-    find_by_attribute(:credit_card_number, credit_card_number)
+    transactions.find {|transaction| transaction.credit_card_number == credit_card_number}
   end
 
   def find_all_by_credit_card_number(credit_card_number)
-    find_all_by_attribute(:credit_card_number, credit_card_number)
+    transactions.find_all {|transaction| transaction.credit_card_number == credit_card_number}
   end
 
   def find_by_credit_card_expiration_date(credit_card_expiration_date)
-    find_by_attribute(:credit_card_expiration_date, credit_card_expiration_date)
+    transactions.find {|transaction| transaction.credit_card_expiration_date == credit_card_expiration_date}
   end
 
   def find_all_by_credit_card_expiration_date(credit_card_expiration_date)
-    find_all_by_attribute(:credit_card_expiration_date, credit_card_expiration_date)
+    transactions.find_all {|transaction| transaction.credit_card_expiration_date == credit_card_expiration_date}
   end
 
   def find_by_result(result)
-    find_by_attribute(:result, result)
+    transactions.find {|transaction| transaction.result == result}
   end
 
   def find_all_by_result(result)
-    find_all_by_attribute(:result, result)
+    transactions.find_all {|transaction| transaction.result == result}
   end
 
   private
-
-  def find_by_attribute(attribute, value)
-    transactions.find {|transaction| transaction.public_send(attribute) == value}
-  end
-
-  def find_all_by_attribute(attribute, value)
-    transactions.find_all {|transaction| transaction.public_send(attribute) == value}
-  end
 
   def inspect
     "#<#{self.class} #{transactions.size} rows>"
