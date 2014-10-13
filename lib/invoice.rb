f517cd8085d230f3b35f8e14d5a32fdba0b80a0b
Travis Yoder <trayoda@gmail.com>
2014-10-13 14:45:15 -0600

    refactored invoice to make it more clear what’s calling where

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -30,22 +30,10 @@ class Invoice
   end
 
   def invoice_items
-    repository.engine
-              .invoice_item_repository
-              .find_all_by_invoice_id(id)
+    repository.invoice_items(id)
   end
 
-
   def items
-    invoice_item_set = repository.engine
-                                 .invoice_item_repository
-                                 .find_all_by_invoice_id(id)
-
-    item_set = invoice_item_set.map {|ii| ii.item_id}
-    item_set.map do |item_num|
-      repository.engine
-                .item_repository
-                .find_by_id(item_num)
-    end
+    repository.items(id)
   end
 end
