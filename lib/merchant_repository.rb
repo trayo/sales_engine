ad77c5e29180e5e743ece3cd3829d9e5510b2e45
David Bayendor <dbayendor@gmail.com>
2014-10-14 11:12:08 -0600

    Select all merchants, not merchant_invoices

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -38,7 +38,7 @@ class MerchantRepository
   end
 
   def revenue(date)
-    engine.merchant_invoices.reduce(0) { |sum, merchant| sum + merchant.revenue(date) }
+    merchants.reduce(0) { |sum, merchant| sum + merchant.revenue(date) }
   end
 
   private
