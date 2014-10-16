ea50398f1d0f4041c1f430a0b92de180572dccb5
David Bayendor <dbayendor@gmail.com>
2014-10-16 10:15:29 -0600

    Sanitation test

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -24,19 +24,27 @@ class CustomerRepository
   end
 
   def find_by_first_name(first_name)
-    customers.find { |customer| customer.first_name.downcase == first_name.downcase }
+    customers.find do |customer|
+      customer.first_name.downcase == first_name.downcase
+    end
   end
 
   def find_all_by_first_name(first_name)
-    customers.find_all { |customer| customer.first_name.downcase == first_name.downcase }
+    customers.find_all do |customer|
+      customer.first_name.downcase == first_name.downcase
+    end
   end
 
   def find_by_last_name(last_name)
-    customers.find { |customer| customer.last_name.downcase == last_name.downcase }
+    customers.find do |customer|
+      customer.last_name.downcase == last_name.downcase
+    end
   end
 
   def find_all_by_last_name(last_name)
-    customers.find_all { |customer| customer.last_name.downcase == last_name.downcase }
+    customers.find_all do |customer|
+      customer.last_name.downcase == last_name.downcase
+    end
   end
 
   def invoices(id)
